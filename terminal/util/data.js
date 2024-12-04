7f6d6bc452864d9454c962b9cf186f9f0974e55d
Mihir Sahu <0xmihirsahu@gmail.com>
2024-12-04 22:16:52 +0530

    first commit

---
+++ terminal/util/data.js
@@ -0,0 +1,26 @@
+export const data = {
+    "gdsc": `
+    Google Developer Student Clubs are university based community groups for students interested in Google developer technologies. Students from all undergraduate or graduate programs with an interest in growing as a developer are welcome. By joining a GDSC, students grow their knowledge in a peer-to-peer learning environment and build solutions for local businesses and their community.
+    `,
+    "abesec": `
+    ABES Engineering College is one of the top engineering Colleges of UPTU in Delhi,NCR. AICTE Approved and AKTU Affiliated college.
+    `,
+    "about": `"Hack Haven: Where Innovation Finds its Home!" is a hackathon that brings together the brightest minds from colleges and universities  to create a solution for real world problems. The students will  have the chance to collaborate with like-minded individuals, share knowledge and learn from each other. Throughout the hackathon, you’ll have access to mentors and industry experts, providing invaluable mentorship and guidance.
+    This hackathon isn’t just about the competition, it’s about building lasting connections and forging a network of talented individuals.`,
+    "prizes": "Will be revealed soon",
+    "sponsors": "Will be revealed soon",
+    "team": "Will Reveal Soon",
+    "venue": "TL01-TL02 Kalpana Chawla Block, ABES Engineering College, Campus 1, 19th KM Stone, NH-24, Ghaziabad-201009, U.P.",
+    "date": `8 July, 2023 9:00 AM`,
+    "register":`Will be opening on 15th June 2023`,
+    "schedule": `
+    15 June, 2023:              Announcement
+    16 - 30 June, 2023:         Registrations
+    25 June, 2023:              Idea Submission Begins
+    2 July, 2023:               Teams Announcement
+    8 July, 2023:               Offline Hackathon
+`,
+    "help": `
+       Available Commands are gdsc,abesec,about,register,prizes,sponsors,venue,date,schedule & team         
+    `,
+}
