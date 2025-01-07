3f33c473a6c6d7b2b0f23b0de00671a62c7ae779
Mihir Sahu <114948140+0xmihirsahu@users.noreply.github.com>
2025-01-07 15:49:24 +0530

    Update timer.js

--- assets/timer.js
+++ assets/timer.js
@@ -12,7 +12,7 @@
         mm = String(today.getMonth() + 1).padStart(2, "0"),
         yyyy = today.getFullYear(),
         nextYear = yyyy + 1,
-        dayMonth = "12/21/",
+        dayMonth = "03/15/",
         birthday = dayMonth + yyyy;
 
     today = mm + "/" + dd + "/" + yyyy;
